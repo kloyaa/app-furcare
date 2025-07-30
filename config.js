743c1186a435fd6d13e85ccada7a7c71cf1a0bf0
Kolya Madridano <niks@Kolyas-Mac-mini.local>
2025-07-30 23:42:40 +0800

    init furcare-dev

--- config.js
+++ config.js
@@ -37,7 +37,7 @@ function parseCommandLineArgs() {
 
 function generateBaseUrl(ip, env) {
     if (env === 'dev') {
-        return 'http://ec2-3-107-113-31.ap-southeast-2.compute.amazonaws.com:3432';
+        return 'http://ec2-3-107-113-31.ap-southeast-2.compute.amazonaws.com:3432/api';
     }
     return `http://${ip}:3432/api`;
 }
